df94736debbd39fa3d96da7fddb81340bce7c035
mette.bentsen <mette.bentsen@mpi-bn.mpg.de>
2023-02-15 17:10:35 +0100

    Fixed test

--- tests/test_pypropeller.py
+++ tests/test_pypropeller.py
@@ -4,4 +4,4 @@ from pypropeller import pypropeller
 def test_import():
     """ Test if pypropeller is imported correctly """
 
-    assert pypropeller.__name__ == "pypropeller"
+    assert pypropeller.__name__ == "pypropeller.pypropeller"
