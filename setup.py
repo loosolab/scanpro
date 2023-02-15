c727db93e003c5c353ccf93bebe12d23b7503b63
yousef.alayoubi <yousef.alayoubi@mpi-bn.mpg.de>
2023-02-15 16:58:48 +0100

    add gaussq2 module to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
-from setuptools import setup, find_packages
+import setuptools  # has to be imported befor distutils
+from setuptools import find_packages
+
+from numpy.distutils.core import Extension, setup
+
+flib = Extension(name = 'pypropeller.gaussq2',
+                 extra_compile_args = ['-O3'],
+                 sources = ['pypropeller/gaussq2.pyf','pypropeller/gaussq2.f'], # you may add several modules files under the same extension
+                 )
 
 setup(
     name='pypropeller',
     version='0.2.0',
     packages=find_packages(include=['pypropeller', 'pypropeller.*']),
     install_requires=['pandas',
-                      'statsmodels']
+                      'statsmodels'],
+    ext_modules= [flib]
 )
