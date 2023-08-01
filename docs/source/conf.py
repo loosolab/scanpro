86862fc7cb576f3e0b9079fa6785874a582d74d3
Yousef Alayoubi <yousef.alayoubi@mpi-bn.mpg.de>
2023-08-01 09:41:09 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,7 +50,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 
-autodoc_mock_imports = ['pandas', 'scipy', 'scanpro.scanpro',
+autodoc_mock_imports = ['pandas', 'scipy', 'scanpro.scanpro', 'scanpro',
                         'seaborn', 'statsmodels']
 
 # -- Options for HTML output -------------------------------------------------
