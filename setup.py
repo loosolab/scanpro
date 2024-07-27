479d35862b30aac5adefe3f6f6c05f7321537b94
Yousef Alayoubi <yousef.alayoubi@mpi-bn.mpg.de>
2024-07-27 10:51:20 +0200

    replace numpy.distutils with scikit-build

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 import re
-from setuptools import find_packages, dist  # has to be imported befor distutils
+from setuptools import find_packages, dist  # has to be imported before distutils
 from distutils.command.sdist import sdist
-
-
 # Test if numpy is installed
 try:
-    from numpy.distutils.core import Extension, setup
+    from skbuild import setup
 except Exception:
     # Else, fetch numpy if needed
-    dist.Distribution().fetch_build_eggs(['numpy'])
-    from numpy.distutils.core import Extension, setup
+    dist.Distribution().fetch_build_eggs(['scikit-build'])
+    from skbuild import setup
 
 
 def find_version(file_path):
@@ -19,12 +17,6 @@ def find_version(file_path):
         return version_match.group(1)
     else:
         raise RuntimeError(f"Unable to find version string in {file_path}.")
-
-
-flib = Extension(name='scanpro.gaussq2',
-                 extra_compile_args=['-O3'],
-                 sources=['scanpro/gaussq2.pyf', 'scanpro/gaussq2.f'],  # you may add several modules files under the same extension
-                 )
 
 
 # Readme from git
@@ -45,12 +37,5 @@ setup(
     license='MIT',
     packages=find_packages(include=['scanpro', 'scanpro.*']),
     cmdclass={'sdist': sdist},
-    setup_requires=['numpy==1.26.4'],
-    install_requires=['pandas',
-                      'statsmodels',
-                      'matplotlib',
-                      'numpy==1.26.4',  # included since numpy 2.0 produce error with pandas
-                      'seaborn',
-                      'patsy',  # for creating design matrices
-                      ],
-    ext_modules=[flib])
+    setup_requires=['scikit-build>=0.13'],
+    )
